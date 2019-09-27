dd88bb9946bc62014b787eab81fb4d9982dd17dc
Arthur <gloriouslordbear@outlook.com>
2019-09-27 15:04:04 -0300

    comentarios

--- alex.cpp
+++ alex.cpp
@@ -5,161 +5,179 @@
 
 using namespace std;
 
-char* start(ifstream &src) {
+int start(ifstream &src, int &line) {
     string lexema;
     lexema = "";
-    char* token;
+    int token;
 
     char c;
     src >> noskipws >> c;
-    if (regex_match(&c, regex("(\d)"))) {
+    while (c == ' ' || c == '\n' || c == '\t') {
+        if (c == '\n') line++;
+        if (c == '/') {
+            src >> noskipws >> c;
+            if (c == '*') {
+                comments(src, line);
+            }
+            else return 0; // divisão
+        }
+        src >> noskipws >> c; 
+    }
+    if (regex_match(&c, regex("\d"))) {
         lexema.append(&c);
         token = integer(src, lexema);
     }
-    else if (regex_match(&c, regex("\w")) {
+    else if (regex_match(&c, regex("\w"))) {
         lexema.append(&c);
-        token = word(src, lexema);
+        token = words(src, lexema);
     }
-    else if (regex_match(&c, regex("\=")) {
+    else if (regex_match(&c, regex("="))) {
         lexema.append(&c);
         token = equals(src, lexema);
 
     } 
-    else if (regex_match(&c, regex("(<|>|!)")) {
-        lexema.append(c);
-        token = comparisons(src, lexema);
+    else if (regex_match(&c, regex("(<|>|!)"))) {
+        lexema.append(&c);
         switch (c)
         {
         case '>':
-            token = maior(src, lexema);
+            token = greater_than(src, lexema);
             break;
         case '<':
-            token = menor(src, lexema);
+            token = lesser_than(src, lexema);
             break;
         case '!':
-            token = diferente(src, lexema);
+            token = not_equal(src, lexema);
             break;
         default:
-            token = "ERRO";
+            token = 0;
             break;
         }
     }
-    else if (c == '/') {
-        // TODO: Comentario ou divisao
-    }
-
     else {
-        switch (c)
-        {
-        case '(':
-            token = '(';
-            break;
-        case ')':
-            token = ')';
-            break;
-        case ',':
-            token = ',';
-            break;
-        case ';':
-            token = ';';
-            break;
-        case '+':
-            token = '+';
-            break;
-        case '-':
-            token = '-';
-            break;
-        case '*':
-            token = '*';
-            break;
-        case '{':
-            token = '{';
-            break;
-        case '}':
-            token = '}';
-            break;
-        default:
-            token = "ERRO";
-            break;
+        switch (c) {
+            case '(':
+                token = 0;
+                break;
+            case ')':
+                token = 0;
+                break;
+            case ',':
+                token = 0;
+                break;
+            case ';':
+                token = 0;
+                break;
+            case '+':
+                token = 0;
+                break;
+            case '-':
+                token = 0;
+                break;
+            case '*':
+                token = 0;
+                break;
+            case '{':
+                token = 0;
+                break;
+            case '}':
+                token = 0;
+                break;
+            case '/':
+                token = 0;
+                break;
+            default:
+                token = 0;
+                break;
         }
     }
-
 }
 
-char* integer(ifstream &src, string lexema) {
+int integer(ifstream &src, string lexema) {
     char c;
     src >> noskipws >> c;
 
-    if ( regex_match(lexema, regex("(0)")) && c == 'h'){
+    if (regex_match(lexema, regex("(0)")) && c == 'h') {
         lexema.append(&c);
         src >> noskipws >> c;
-        if (!regex_match(&c, regex("[a-fA-F]"))) return "ERRO";
+        if (!regex_match(&c, regex("[0-9a-fA-F]"))) return 0;
         lexema.append(&c);
         src >> noskipws >> c;
-        if (!regex_match(&c, regex("[a-fA-F]"))) return "ERRO";
+        if (!regex_match(&c, regex("[0-9a-fA-F]"))) return 0;
         lexema.append(&c);
-        return "Hexadecimal";
-
+        return 0;
     }
+
     while(regex_match(&c, regex("\d"))) {
         lexema.append(&c);
         src >> noskipws >> c;
     }
-    src.seekg(-1, ios::cur);
-    return "Int";
+    src.seekg(-1, ios::cur); // devolve c
+    return 0;
 }
 
-char* word(ifstream &src, string lexema){
+int words(ifstream &src, string lexema){
     char c;
     src >> noskipws >> c;
     while (regex_match(&c, regex("\w"))) {
-        lexema.append(c);
+        lexema.append(&c);
         src >> noskipws >> c;
     }
     // match tabela de símbolos para palavras reservadas
     src.seekg(-1, ios::cur);
-    return "LEXEMA";
+    return 0;
 }
 
-char* equals(ifstream & src, string lexema) {
+int equals(ifstream & src, string lexema) {
     char c;
     src >> noskipws >> c;
     if (c == '=') {
         lexema.append(&c);
-        return 'IGUALDADE';
+        return 0;
     }
     src.seekg(-1, ios::cur);
-    return "ATRIBUICAO";
+    return 0;
 }
 
-char* maior(ifstream &src, string lexema) {
+int greater_than(ifstream &src, string lexema) {
     char c;
     src >> noskipws >> c;
-    if (regex_match(&c, regex("\="))) {
-        lexema.append(c);
-        return ">=";
+    if (regex_match(&c, regex("="))) {
+        lexema.append(&c);
+        return 0;
     }
     src.seekg(-1, ios::cur);
-    return "MAIOR"
+    return 0;
 }
 
-char* menor(ifstream &src, string lexema) {
+int lesser_than(ifstream &src, string lexema) {
     char c;
     src >> noskipws >> c;
-    if (regex_match(&c, regex("\="))) {
-        lexema.append(c);
-        return "<=";
+    if (regex_match(&c, regex("="))) {
+        lexema.append(&c);
+        return 0;
     }
     src.seekg(-1, ios::cur);
-    return "MENOR";
+    return 0;
 }
 
-char* diferente(ifstream &src, string lexema) {
+int not_equal(ifstream &src, string lexema) {
     char c;
     src >> noskipws >> c;
-    if (regex_match(&c, regex("\="))) {
-        lexema.append(c);
-        return "!=";
+    if (regex_match(&c, regex("="))) {
+        lexema.append(&c);
+        return 0;
     }
-    return "ERRO";
+    return 0;
+}
+
+void comments(ifstream &src, int &line) {
+    char c;
+    src >> noskipws >> c;
+    while(true) {
+        if (c == '*') {
+            src >> noskipws >> c;
+            if (c == '/') break;
+        }
+    }
 }
