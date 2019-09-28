c83e4f953cc9dac9a6b0441677fafd4582a898f5
Arthur <gloriouslordbear@outlook.com>
2019-09-28 14:28:11 -0300

    last commit but right now

---
+++ alexa.cpp
@@ -0,0 +1,197 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include "symbols.cpp"
+#include "DICIONARIO.h"
+
+using namespace std;
+
+extern tabela_simbolos table;
+
+int start(ifstream &src, int &line) {
+    string lexema;
+    lexema = "";
+    int token;
+
+    char c;
+    src >> noskipws >> c;
+    while (c == ' ' || c == '\n' || c == '\t') {
+        if (c == '\n') line++;
+        if (c == '/') { // comentario ou divisao
+            src >> noskipws >> c;
+            if (c == '*') {
+                comments(src, line);
+            }
+            else return T_DIVIDE; // divisão
+        }
+        src >> noskipws >> c;
+    }
+    if (regex_match(&c, regex("\d"))) {
+        lexema.append(&c);
+        token = number(src, lexema);
+    }
+    else if (regex_match(&c, regex("\w"))) {
+        lexema.append(&c);
+        token = words(src, lexema);
+    }
+    else if (c == '_') {
+        lexema.append(&c);
+        src >> noskipws >> c;
+        while (c == '_') {
+            lexema.append(&c);
+            src >> noskipws >> c;
+        }
+        if (regex_match(&c, regex("\d|\w"))){
+            lexema.append(&c);
+            token = words(src, lexema);
+        }
+        else return LEX_ERROR;
+    }
+    else if (c == '=') {
+        lexema.append(&c);
+        token = equals(src, lexema);
+
+    } 
+    else if (regex_match(&c, regex("(<|>|!)"))) {
+        lexema.append(&c);
+        switch (c) {
+            case '>':
+                token = greater_than(src, lexema);
+                break;
+            case '<':
+                token = lesser_than(src, lexema);
+                break;
+            case '!':
+                token = not_equal(src, lexema);
+                break;
+            default:
+                token = LEX_ERROR;
+                break;
+        }
+    }
+    else {
+        switch (c) {
+            case '(':
+                token = T_OPEN_PARENTESIS;
+                break;
+            case ')':
+                token = T_CLOSE_PARENTESIS;
+                break;
+            case ',':
+                token = T_COMMA;
+                break;
+            case ';':
+                token = T_SEMICOLON;
+                break;
+            case '+':
+                token = T_ADD;
+                break;
+            case '-':
+                token = T_SUBTRACT;
+                break;
+            case '*':
+                token = T_MULTIPLY;
+                break;
+            default:
+                token = LEX_ERROR;
+                break;
+        }
+    }
+    return token;
+}
+
+int number(ifstream &src, string lexema) {
+    char c;
+    src >> noskipws >> c;
+
+    if (regex_match(lexema, regex("(0)")) && c == 'h') {
+        lexema.append(&c);
+        src >> noskipws >> c;
+        if (!regex_match(&c, regex("[0-9a-fA-F]"))) return 0;
+        lexema.append(&c);
+        src >> noskipws >> c;
+        if (!regex_match(&c, regex("[0-9a-fA-F]"))) return 0;
+        lexema.append(&c);
+        return T_VALUE;
+    }
+
+    while(regex_match(&c, regex("\d"))) {
+        lexema.append(&c);
+        src >> noskipws >> c;
+    }
+    src.seekg(-1, ios::cur); // devolve c
+    return T_VALUE;
+}
+
+int words(ifstream &src, string lexema){
+    char c;
+    src >> noskipws >> c;
+    while (regex_match(&c, regex("(\w|\d|_)"))) {
+        lexema.append(&c);
+        src >> noskipws >> c;
+    }
+    
+    int token = table.eh_palavra_reservada(lexema);
+    if (token == 101) {
+        table.add_id(lexema);
+    }
+    src.seekg(-1, ios::cur);
+    return token;
+}
+
+int equals(ifstream & src, string lexema) {
+    char c;
+    src >> noskipws >> c;
+    if (c == '=') {
+        lexema.append(&c);
+        return T_EQUALS;
+    }
+    src.seekg(-1, ios::cur);
+    return T_ATRIBUICAO;
+}
+
+int greater_than(ifstream &src, string lexema) {
+    char c;
+    src >> noskipws >> c;
+    if (regex_match(&c, regex("="))) {
+        lexema.append(&c);
+        return T_GREATER_THAN_EQUAL;
+    }
+    src.seekg(-1, ios::cur);
+    return T_GREATER_THAN;
+}
+
+int lesser_than(ifstream &src, string lexema) {
+    char c;
+    src >> noskipws >> c;
+    if (regex_match(&c, regex("="))) {
+        lexema.append(&c);
+        return T_LESSER_THAN_EQUAL;
+    }
+    src.seekg(-1, ios::cur);
+    return T_LESSER_THAN;
+}
+
+int not_equal(ifstream &src, string lexema) {
+    char c;
+    src >> noskipws >> c;
+    if (regex_match(&c, regex("="))) {
+        lexema.append(&c);
+        return T_NOT_EQUAL;
+    }
+    return LEX_ERROR;
+}
+
+void comments(ifstream &src, int &line) {
+    char c;
+    src >> noskipws >> c;
+    while(true) {
+        if (c == '\n') line++;
+        if (c == '*') {
+            src >> noskipws >> c;
+            if (c == '/') break;
+        }
+        src >> noskipws >> c;
+    }
+}
---
+++ symbols.cpp
@@ -0,0 +1,120 @@
+#include <unordered_map>
+#include <iostream>
+#include <regex>
+#include "DICIONARIO.h"
+using namespace std;
+
+class tabela_simbolos{
+
+private:
+  unordered_map<string,int> symbols;
+  int posicao;
+
+public:
+  tabela_simbolos(){
+    this->symbols["const"]=1;
+    this->symbols["integer"]=2;
+    this->symbols["byte"]=3;
+    this->symbols["string"]=4;
+    this->symbols["while"]=5;
+    this->symbols["if"]=6;
+    this->symbols["else"]=7;
+    this->symbols["and"]=8;
+    this->symbols["or"]=9;
+    this->symbols["not"]=10;
+    this->symbols["="]=11;
+    this->symbols["=="]=12;
+    this->symbols["("]=13;
+    this->symbols[")"]=14;
+    this->symbols["<"]=15;
+    this->symbols[">"]=16;
+    this->symbols["!="]=17;
+    this->symbols[">="]=18;
+    this->symbols["<="]=19;
+    this->symbols[","]=20;
+    this->symbols["+"]=21;
+    this->symbols["-"]=22;
+    this->symbols["*"]=23;
+    this->symbols["/"]=24;
+    this->symbols[";"]=25;
+    this->symbols["begin"]=26;
+    this->symbols["end"]=27;
+    this->symbols["then"]=28;
+    this->symbols["readln"]=29;
+    this->symbols["write"]=30;
+    this->symbols["writeln"]=31;
+    this->symbols["main"]=32;
+    this->symbols["boolean"]=33;
+    this->symbols["true"]=34;
+    this->symbols["false"]=34;
+    this->symbols["id"]=35;
+
+    this->posicao = 36;
+
+  }//fim construtor
+
+
+  int eh_palavra_reservada(string string){
+
+    for (auto it = symbols.cbegin(); it != symbols.cend(); ++it)
+        if(string == (*it).first) return (*it).second;
+
+    return T_NEW_ID;
+
+  }// fim eh_palavra_reservada
+
+  void add_id(string id){
+
+    this->symbols[id] = 35;
+
+  }//fim add_id
+
+  void add_valor(string valor){
+
+    this->symbols[valor] = 34;
+
+  }//fim add_valor
+
+  void add_string(string string){
+
+    this->symbols[string] = 34;
+
+  }//fim add_string
+
+    void print_tabela(){
+      //debbug
+      for (auto it = symbols.cbegin(); it != symbols.cend(); ++it)
+        cout << " [" << (*it).first << ':' << (*it).second << ']' << endl;
+
+    }//fim print_tabela
+
+
+
+};//fim classe tabela_simbolos
+
+// int main(){
+
+//   /*map<string, int> m;
+//   m["hello"] = 23;
+//   // check if key is present
+//   if (m.find("hello") != m.end())
+//     cout << "map contains key\n";
+//   // retrieve
+//   cout << m["hello"] << '\n';
+//   map<string, int>::iterator i = m.find("hello");
+//   assert(i != m.end());
+//   cout << "Key: " << i->first << " Value: " << i->second << '\n';
+//   return 0;
+//   */
+//   tabela_simbolos symbols;//instancia da tabela_simbolos
+//   symbols.add_id("mago");
+//   symbols.add_valor("42");
+//   symbols.add_string("estudante");
+//   symbols.add_string("esferico");
+//   symbols.print_tabela();
+//   bool r = symbols.eh_palavra_reservada("while");
+//   cout << r << endl;
+//   r = symbols.eh_palavra_reservada("hu3");
+//   cout << r << endl;
+
+// }//fim main()
