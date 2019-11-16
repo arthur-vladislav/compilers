77f4d2465816381d603ca67b61fa8cb98aa8620c
Henrique Schiess Pertussati <45759325+ICESchiess@users.noreply.github.com>
2019-11-16 17:57:23 -0300

    Add files via upload

--- symbols.cpp
+++ symbols.cpp
@@ -1,121 +1,170 @@
+/*
+Arthur Pereira
+Henrique Schiess Pertussati
+Ian Paixão
+*/
 #include <unordered_map>
 #include <iostream>
+#include <regex>
 #include "DICIONARIO.h"
+#define NO_TYPE 0
+#define TYPE_BOOLEAN 1
+#define T_NEW_ID 101
+#define TYPE_INTEGER 2
+typedef unsigned char BYTE;
 using namespace std;
 
-class tabela_simbolos{
 
-private:
-  unordered_map<string, int> symbols;
-  unordered_map<string, string> constantes;
-  int posicao;
+class ID {
 
 public:
-  tabela_simbolos(){
-    this->symbols["const"]=1;
-    this->symbols["integer"]=2;
-    this->symbols["byte"]=3;
-    this->symbols["string"]=4;
-    this->symbols["while"]=5;
-    this->symbols["if"]=6;
-    this->symbols["else"]=7;
-    this->symbols["and"]=8;
-    this->symbols["or"]=9;
-    this->symbols["not"]=10;
-    this->symbols["="]=11;
-    this->symbols["=="]=12;
-    this->symbols["("]=13;
-    this->symbols[")"]=14;
-    this->symbols["<"]=15;
-    this->symbols[">"]=16;
-    this->symbols["!="]=17;
-    this->symbols[">="]=18;
-    this->symbols["<="]=19;
-    this->symbols[","]=20;
-    this->symbols["+"]=21;
-    this->symbols["-"]=22;
-    this->symbols["*"]=23;
-    this->symbols["/"]=24;
-    this->symbols[";"]=25;
-    this->symbols["begin"]=26;
-    this->symbols["end"]=27;
-    this->symbols["then"]=28;
-    this->symbols["readln"]=29;
-    this->symbols["write"]=30;
-    this->symbols["writeln"]=31;
-    this->symbols["main"]=32;
-    this->symbols["boolean"]=33;
-    this->symbols["true"]=34;
-	  this->constantes["true"] = "boolean";
-    this->symbols["false"]=34;
-	  this->constantes["false"] = "boolean";
-    this->symbols["id"]=35;
 
-    this->posicao = 36;
+	char classe; // v = variavel; c =const; b = blank
+	int token;
+	int tipo;
 
-  }//fim construtor
+	ID() {
+
+	}
+
+	ID(char classe, int token, int tipo) {
+
+		this->classe = classe;
+		this->token = token;
+		this->tipo = tipo;
 
 
-  int eh_palavra_reservada(string string){
+	}//fim construtor
 
-    for (auto it = symbols.cbegin(); it != symbols.cend(); ++it)
-        if(string == (*it).first) return (*it).second;
 
-    return T_NEW_ID;
-  }// fim eh_palavra_reservada
+};
 
-  void add_id(string id) {
+class tabela_simbolos {
 
-    this->symbols[id] = 35;
+private:
 
-  }//fim add_id
+public:
 
-  void add_valor(string valor){
+	unordered_map<string, ID> symbols;
+	unordered_map<string, int> valores_int;
+	unordered_map<string, BYTE> valores_byte;
+	unordered_map<string, string> valores_string;
+	unordered_map<string, bool> valores_bool;
+	string ultimo_adicionado;
 
-    this->symbols[valor] = 34;
-	this->constantes[valor] = "integer";
+	tabela_simbolos() {
+		this->symbols["const"] = ID('b', 1, NO_TYPE);
+		this->symbols["integer"] = ID('b', 2, NO_TYPE);
+		this->symbols["byte"] = ID('b', 3, NO_TYPE);
+		this->symbols["string"] = ID('b', 4, NO_TYPE);
+		this->symbols["while"] = ID('b', 5, NO_TYPE);
+		this->symbols["if"] = ID('b', 6, NO_TYPE);
+		this->symbols["else"] = ID('b', 7, NO_TYPE);
+		this->symbols["and"] = ID('b', 8, NO_TYPE);
+		this->symbols["or"] = ID('b', 9, NO_TYPE);
+		this->symbols["not"] = ID('b', 10, NO_TYPE);
+		this->symbols["="] = ID('b', 11, NO_TYPE);
+		this->symbols["=="] = ID('b', 12, NO_TYPE);
+		this->symbols["("] = ID('b', 13, NO_TYPE);
+		this->symbols[")"] = ID('b', 14, NO_TYPE);
+		this->symbols["<"] = ID('b', 15, NO_TYPE);
+		this->symbols[">"] = ID('b', 16, NO_TYPE);
+		this->symbols["!="] = ID('b', 17, NO_TYPE);
+		this->symbols[">="] = ID('b', 18, NO_TYPE);
+		this->symbols["<="] = ID('b', 19, NO_TYPE);
+		this->symbols[","] = ID('b', 20, NO_TYPE);
+		this->symbols["+"] = ID('b', 21, NO_TYPE);
+		this->symbols["-"] = ID('b', 22, NO_TYPE);
+		this->symbols["*"] = ID('b', 23, NO_TYPE);
+		this->symbols["/"] = ID('b', 24, NO_TYPE);
+		this->symbols[";"] = ID('b', 25, NO_TYPE);
+		this->symbols["begin"] = ID('b', 26, NO_TYPE);
+		this->symbols["end"] = ID('b', 27, NO_TYPE);
+		this->symbols["then"] = ID('b', 28, NO_TYPE);
+		this->symbols["readln"] = ID('b', 29, NO_TYPE);
+		this->symbols["write"] = ID('b', 30, NO_TYPE);
+		this->symbols["writeln"] = ID('b', 31, NO_TYPE);
+		this->symbols["main"] = ID('b', 32, NO_TYPE);
+		this->symbols["boolean"] = ID('b', 33, NO_TYPE);
+		this->symbols["true"] = ID('c', 34, TYPE_BOOLEAN);
+		this->symbols["false"] = ID('c', 34, TYPE_BOOLEAN);
 
-  }//fim add_valor
+		ultimo_adicionado = "false";
 
-  void add_string(string string){
+	}//fim construtor
 
-    this->symbols[string] = 34;
-	this->constantes[string] = "string";
 
-  }//fim add_string
+	int eh_palavra_reservada(string string) {
 
-    void print_tabela(){
-      //debbug
-      for (auto it = symbols.cbegin(); it != symbols.cend(); ++it)
-        cout << " [" << (*it).first << ':' << (*it).second << ']' << endl;
-	  for (auto it = constantes.cbegin(); it != constantes.cend(); ++it)
-		  cout << " [" << (*it).first << ':' << (*it).second << ']' << endl;
+		for (auto it = symbols.cbegin(); it != symbols.cend(); ++it)
+			if (string == (*it).first) return (*it).second.token;
 
-    }//fim print_tabela
+		return T_NEW_ID;
+		//return 0;
+
+	}// fim eh_palavra_reservada
+
+	void add_id(string id, char classe, int tipo) {
+
+		this->symbols[id] = ID(classe, 35, tipo);
+		ultimo_adicionado = id;
+
+	}//fim add_id
+
+
+	/*
+	void add_valor(string valor){
+
+	  this->symbols[valor] = 34;
+	  this->constantes[valor] = "integer";
+
+	}//fim add_valor
+
+	void add_string(string string){
+
+	  this->symbols[string] = 34;
+	  this->constantes[string] = "string";
+
+	}//fim add_string
+	*/
+	void print_tabela() {
+		//debbug
+		for (auto it = symbols.cbegin(); it != symbols.cend(); ++it)
+			cout << " [" << (*it).first << ':' << (*it).second.classe << ']' << endl;
+
+	}//fim print_tabela
 
 
 
 };//fim classe tabela_simbolos
 
-//int main(){
-//
-//   /*map<string, int> m;
-//   m["hello"] = 23;
-//   // check if key is present
-//   if (m.find("hello") != m.end())
-//     cout << "map contains key\n";
-//   // retrieve
-//   cout << m["hello"] << '\n';
-//   map<string, int>::iterator i = m.find("hello");
-//   assert(i != m.end());
-//   cout << "Key: " << i->first << " Value: " << i->second << '\n';
-//   return 0;
-//   */
-//tabela_simbolos symbols;
-//symbols.add_tipo("42");//instancia da tabela_simbolos
- //   symbols.print_tabela();
-//   bool r = symbols.eh_palavra_reservada("while");
-//   cout << r << endl;
-//   r = symbols.eh_palavra_reservada("hu3");
-//   cout << r << endl;
-//}//fim main()
+/*
+int main() {
+
+	/*
+	map<string, int> m;
+	m["hello"] = 23;
+	// check if key is present
+	if (m.find("hello") != m.end())
+	  cout << "map contains key\n";
+	// retrieve
+	cout << m["hello"] << '\n';
+	map<string, int>::iterator i = m.find("hello");
+	assert(i != m.end());
+	cout << "Key: " << i->first << " Value: " << i->second << '\n';
+	return 0;
+	
+
+	tabela_simbolos symbols;
+	symbols.add_id("42", 'b', 0);//instancia da tabela_simbolos
+	symbols.print_tabela();
+	bool r = symbols.eh_palavra_reservada("while");
+	cout << r << endl;
+	r = symbols.eh_palavra_reservada("hu3");
+	cout << r << endl;
+	symbols.symbols.at(symbols.ultimo_adicionado) = ID('c', 35, TYPE_INTEGER);
+	symbols.print_tabela();
+
+
+}
+*/
